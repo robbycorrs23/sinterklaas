20f37feef294279ee647e309526a2fbf98e0e7d2
Robert Corrado <jhello7@pm.me>
2021-12-10 20:42:29 -0500

    Finally fixed the handleClick functionality, also added input

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,58 +1,81 @@
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
+import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
 
-// let intervalId
-let calorieTarget = 5000
-let homesRemaining = 1000
-let totalHomes = 0
-const cookiesMilk = 75
-const carrotsTea = -50
-let calorieCount = 0
+let calorieCount = 0;
+const cookiesMilk = 15;
+const carrotsTea = -10;
+let currentSpeed = 10;
+let homesRemaining = 0;
 
 export default function Main() {
-    const [count, setCount] = useState(0)
-    const [intervalId, setIntervalId] = useState(0)
+    const [intervalId, setIntervalId] = useState(false)
+    // const [homesDisplay, setHomesDisplay] = useState(200)
+    const [totalHomes, setTotalHomes] = useState(0)
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCount(prevCount => prevCount + 1)
-        }, 1000)
+    let calorieTarget = 5000
+    let interval;
 
-        return () => clearInterval(intervalId)
-    }, [intervalId])
 
     const handleClick = () => {
-        const santaDeliver = setIntervalId(deliverNow)
-    }  
-
-    const deliverNow = () => {
-        const currentSpeed = (calorieCount < 5000) ? 10 : 5
-        const nextTen = [
-            ...Array(currentSpeed)
-        ]
-        .map(() => 
-            Math.round(Math.random()) === 1 && calorieCount < 4750 ? cookiesMilk : 
-            Math.round(Math.random()) === 1 && calorieCount > 4750 ? Math.floor(cookiesMilk * 0.5) : carrotsTea )
-            .reduce((acc, cur) => acc + cur)
-            calorieCount += nextTen
-            totalHomes += currentSpeed
-            homesRemaining -= currentSpeed
-            // homesRemaining === 0 ? clearInterval(intervalId) : null
-    // }
-    // // const santaDeliver = () => {
-    // //     intervalId = setInterval(deliverNow, 1000)
+        if (homesRemaining !== 0) {
+            interval = setInterval(() => {
+                calorieCount < 5000 && homesRemaining > 10 ? currentSpeed = 10 : 
+                calorieCount > 5000 && homesRemaining > 10 ? currentSpeed = 5 :
+                currentSpeed = 1
+                const nextTen =
+                    [...Array(currentSpeed)]
+                        .map(() =>
+                            Math.round(Math.random()) === 1 && calorieCount < 4750 ? cookiesMilk :
+                                Math.round(Math.random()) === 1 && calorieCount > 4750 ? Math.floor(cookiesMilk * 0.5) :
+                                    carrotsTea
+                        )
+                        .reduce((acc, cur) => acc + cur);
+                calorieCount += nextTen;
+                setTotalHomes(prev => prev += currentSpeed);
+                homesRemaining -= currentSpeed
+                stopSanta()
+            }, 1000)
+        } else {
+            alert("Please enter value for target # of homes!")
+        }
     }
+
+    const stopSanta = () => {
+        if (homesRemaining === 0) { 
+            clearInterval(interval)
+         }
+    }
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        homesRemaining = e.target.value
+    }
+    
     return (
         <div className="main">
-          <h1>The component has been rendered for {count} seconds</h1>
           <h1>Santa Dashboard</h1>
           <p>Total Homes: {totalHomes}</p>
-          <button onClick={handleClick}>
+          <button 
+          onClick={() => handleClick()}
+          >
             {intervalId ? "Stop counting" : "Start counting"}
           </button>
           <p>Nightly calorie target: {calorieTarget}</p>
-          {/* <p>Current speed: {currentSpeed}</p>  */}
-          <p>Calorie count: {calorieCount}</p>
-          <p>Homes remaining: {homesRemaining}</p>
+          <p>Current speed: {currentSpeed} homes per second</p> 
+          <p>Calorie count: {calorieCount < 0 ? 0 : calorieCount}</p>
+          <p>Homes remaining: {interval ? "0" : homesRemaining}</p>
+            <InputGroup className="mt-2">
+                <InputGroup.Text id="basic-addon1">Target</InputGroup.Text>
+                <Form.Control
+                    className="p-2 text-green-400"
+                    placeholder="# of homes to visit"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    type="text"
+                    onChange={e => handleChange(e)}
+                />
+            </InputGroup>
         </div>
       );
 }
